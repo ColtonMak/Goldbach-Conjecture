7b60e36b7a2bbe2696997625e2361d698d2860ff
Colton Mak <colton.mak1009@gmail.com>
2018-04-16 14:34:16 -0400

    Added goldbach partition

--- src/Goldbach.java
+++ src/Goldbach.java
@@ -4,25 +4,43 @@ public class Goldbach {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		int input = sc.nextInt();
 		sc.close();
 		
-		boolean[] primes = new boolean[num+1];
-		for (int i = 2; i < primes.length; i++) {
-			primes[i] = true;
+		boolean[] primeChecker = new boolean[input+1];
+		for (int i = 2; i < primeChecker.length; i++) {
+			primeChecker[i] = true;
 		}
 		
-		for (int i = 2; i < (int)Math.sqrt(num); i++) {
-			if (primes[i] == true) {
-				for (int j = (int) Math.pow(i, 2); j < primes.length; j+=i) {
-					primes[j] = false;
+		for (int i = 2; i < (int)Math.sqrt(input); i++) {
+			if (primeChecker[i] == true) {
+				for (int j = (int) Math.pow(i, 2); j < primeChecker.length; j+=i) {
+					primeChecker[j] = false;
 				}
 			}
 		}
 		
+		int primeCount = 0;
+		for (int i = 0; i < primeChecker.length; i++) {
+			if (primeChecker[i] == true) {
+				primeCount++;
+			}
+		}
+		
+		int[] primes = new int[primeCount];
+		int j = 0;
+		for (int i = 0; i < primeChecker.length; i++) {
+			if (primeChecker[i] == true) {
+				primes[j] = i;
+				j++;
+			}
+		}
 		for (int i = 0; i < primes.length; i++) {
-			if (primes[i] == true) {
-				System.out.println(i);
+			int num = input - primes[i];
+			for (int k = 0; k <= primes.length / 2; k++) {
+				if (num == primes[k]) {
+					System.out.println(input + " = " + primes[k] + " + " + primes[i]);
+				}
 			}
 		}
 	}
